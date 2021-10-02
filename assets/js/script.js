fe577630219d1eb93462651691964eb60d276c28
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-10-02 12:20:55 -0500

    added corret classes to elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+var theDate = function(){
 var currentTime = moment().format("MMMM Do, YYYY - hh:mm:ss a");
 var currentDay = document.querySelector("#currentDay")
 currentDay.textContent = currentTime;
+}
+
+var timeBox = document.querySelector("#textBox");
+
+var timeColor = function() {
+    var currentTime = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+  
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+theDate()
+setInterval(theDate, 1000);
