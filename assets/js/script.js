5958cccbbb0da018a0e2c77403df922bab9c086c
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-10-02 13:17:30 -0500

    added colors but still need to fix classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,20 @@ currentDay.textContent = currentTime;
 var timeBox = document.querySelector("#textBox");
 
 var timeColor = function() {
-    var currentTime = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-  
+    var currentTime = moment().hours()
 
+  
+$(".time-block").each(function(){
+    var textArea = parseInt($(this).attr("id")[1]);
+    if (textArea < currentTime) {
+        $(this).addClass("past");
+    } else if  (textArea === currentTime){
+        $(this).addClass("present");
+    } else
+     {
+        $(this).addClass("future");
+    }
+})
     
 }
 
@@ -28,3 +39,4 @@ var timeColor = function() {
 
 theDate()
 setInterval(theDate, 1000);
+timeColor()
